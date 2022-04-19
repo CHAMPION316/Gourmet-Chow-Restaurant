0d4133356b037ec6764ea6c95593473d97bc35be
C316 <royer.seguracalderon@gmail.com>
2022-04-19 19:07:09 +0000

    add booking function

--- chow/views.py
+++ chow/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def main_page(request):
     return render(request, 'index.html')
+
+def booking_page(request):
+    return render(request, 'book.html')
