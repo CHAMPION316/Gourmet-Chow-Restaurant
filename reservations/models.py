f60e640458dc3fa84e5cf08b36a102eca88bf26a
C316 <royer.seguracalderon@gmail.com>
2022-04-23 13:10:14 +0000

    Delete commented out code

--- reservations/models.py
+++ reservations/models.py
@@ -11,31 +11,3 @@ class booking(models.Model):
     fname=models.CharField(max_length=30)
     lname=models.CharField(max_length=30)
     booking_start=models.DateField()
-
-
-# class Guest(models.Model):
-
-#     class GuestCount(models.TextChoices):
-#         GUEST1 = 'G1', _('Guest 1')
-#         GUEST2 = 'G2', _('Guest 2')
-#         GUEST3 = 'G3', _('Guest 3')
-#         GUEST4 = 'G4', _('Guest 4')
-#         GUEST5 = 'G5', _('Guest 5')
-#         GUEST6 = 'G6', _('Guest 6')
-
-#     guest_count = models.CharField(
-#         max_length=6,
-#         choices=GuestCount.choices,
-#         default=GuestCount.GUEST1,
-#         null=False,
-#         blank=False,
-#     )
-
-#     def is_highcount(self):
-#         return self.guest_count in {
-#             self.GuestCount.GUEST4,
-#             self.GuestCount.GUEST5,
-#         }
-
-#     def __str__(self):
-#         return self.guest_count
