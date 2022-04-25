c3ac2b1b40edf94c130bcacb641d1cb61d8a3bcb
C316 <royer.seguracalderon@gmail.com>
2022-04-25 01:47:33 +0000

    Update paths, incorrect folder path for nav links

--- reservations/urls.py
+++ reservations/urls.py
@@ -9,5 +9,10 @@ from reservations import views
 
 urlpatterns = [
     path('', views.view_home, name='home'),
+    path('menu/', views.view_menu, name='menu'),
     path('add_booking/', views.add_booking, name='add_booking'),
+    path('view_booking/', views.view_booking, name='view_booking'),
+    path('edit/<booking_id>', views.edit_booking, name='edit_booking'),
+    path('delete/<booking_id>', views.delete_booking, name='delete_booking'),
+    path('contact', views.contact, name='contact'),
 ]
