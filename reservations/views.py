f56bcc413abb4dc2fd555c24a8fc7696cdeaae3b
C316 <royer.seguracalderon@gmail.com>
2022-04-25 01:44:03 +0000

    update paths again due to error

--- reservations/views.py
+++ reservations/views.py
@@ -1,27 +1,41 @@
 from django.shortcuts import render
-from .models import booking
-from django.db import connection
-from django.contrib import messages
 
 # Create your views here.
 
 
-def main_page(request):
+# def view_home(request):
+#     """
+#     Function enables user to view the home page.
+#     """
+#     return render(request, 'reservations/index.html')
+
+def view_home(request):
     return render(request, 'index.html')
 
 
-def book_table(request):
-    if request.method=="POST":
-        if request.POST.get('fname') and request.POST.get('lname') and request.POST.get('booking_start'):
-            saveobj=booking()
-            saveobj.fname=request.POST.get('fname')
-            saveobj.lname=request.POST.get('lname')
-            saveobj.booking_start.request.POST.get('booking_start')
-            cursor=connection.cursor("insert into chow316(fname,laname,booking_start) values('"+saveobj.fname+"','"+saveobj.lname+"','"+saveobj.booking_start+"')")
-            messages.success(request,"Your booking "+saveobj.fname+" has been successfully processed.")
-            return render(request, "book.html")
-    else:
-        return render(request, 'book.html')
+
+
+def add_booking(request):
+    return render(request, 'reservations/add_booking.html')
+
+
+
+# def main_page(request):
+#     return render(request, 'index.html')
+
+
+# def book_table(request):
+#     if request.method=="POST":
+#         if request.POST.get('fname') and request.POST.get('lname') and request.POST.get('booking_start'):
+#             saveobj=booking()
+#             saveobj.fname=request.POST.get('fname')
+#             saveobj.lname=request.POST.get('lname')
+#             saveobj.booking_start.request.POST.get('booking_start')
+#             cursor=connection.cursor("insert into chow316(fname,laname,booking_start) values('"+saveobj.fname+"','"+saveobj.lname+"','"+saveobj.booking_start+"')")
+#             messages.success(request,"Your booking "+saveobj.fname+" has been successfully processed.")
+#             return render(request, "book.html")
+#     else:
+#         return render(request, 'book.html')
 
 
 # def booking_page(request):
