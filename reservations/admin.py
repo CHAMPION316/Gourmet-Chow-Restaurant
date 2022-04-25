b9473f2f43863bd0c53f1e3416cc58c964aac5e8
C316 <royer.seguracalderon@gmail.com>
2022-04-25 01:41:00 +0000

    comment out code due to error

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import booking
+# from .models import booking
 
 # Register your models here.
 
-admin.site.register(booking)
+# admin.site.register(add_booking)
