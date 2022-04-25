fdf2d9ef15e95a55ff488811146f429894287d7a
C316 <royer.seguracalderon@gmail.com>
2022-04-25 12:44:21 +0000

    Add BookingAdmin

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,6 +1,20 @@
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
-# from .models import booking
+from .models import Booking
 
-# Register your models here.
 
-# admin.site.register(add_booking)
+@admin.register(Booking)
+class BookingAdmin(ModelAdmin):
+    """
+    Class registered to represent model in admin database.
+    """
+    list_display = ('user', 'customer_name',
+                    'reservation_date_and_time',
+                    'phone_number',
+                    'number_of_customers')
+    search_fields = ('customer_name',
+                     'reservation_date_and_time',
+                     'phone_number')
+    list_filter = ('customer_name',
+                   'reservation_date_and_time',
+                   'phone_number')
