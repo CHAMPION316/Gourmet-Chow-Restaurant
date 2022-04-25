c41ccc540566ace0aeaa5ee9195b24e2bb57ae54
C316 <royer.seguracalderon@gmail.com>
2022-04-25 14:01:49 +0000

    Add required fields for DateTime pick to third field

--- reservations/forms.py
+++ reservations/forms.py
@@ -7,3 +7,21 @@ class BookingForm(forms.ModelForm):
     """
     Class to construct booking form from the model.
     """
+    class Meta:
+        """
+        Add form inputs from the model items as form fields.
+        """
+        model = Booking
+        fields = ('customer_name', 'phone_number',
+                  'reservation_date_and_time', 'number_of_customers')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add class, required field and DateTime picker
+        to third field.
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['reservation_date_and_time'].widget.attrs['class'] = 'form-control datetimepicker-input'
+        self.fields['reservation_date_and_time'].widget = DateTimePicker()
+        self.fields['reservation_date_and_time'].widget.attrs['required'] = 'required'
+        self.fields['phone_number'].widget.attrs['required'] = 'required'
