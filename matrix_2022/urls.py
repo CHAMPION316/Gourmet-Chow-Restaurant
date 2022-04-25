31a830ad2a84edb382d93733a6f31ef17f572417
C316 <royer.seguracalderon@gmail.com>
2022-04-25 01:47:01 +0000

    Update paths, placed in incorrect folder

--- matrix_2022/urls.py
+++ matrix_2022/urls.py
@@ -19,12 +19,7 @@ from django.urls import path, include
 APP_NAME = "reservations"
 
 urlpatterns = [
-    path('', views.view_home, name='home'),
-    path('menu/', views.view_menu, name='menu'),
-    path('add_booking/', views.add_booking, name='add_booking'),
-    path('view_booking/', views.view_booking, name='view_booking'),
-    path('edit/<booking_id>', views.edit_booking, name='edit_booking'),
-    path('delete/<booking_id>', views.delete_booking, name='delete_booking'),
-    path('contact', views.contact, name='contact'),
+    path('admin/', admin.site.urls),
+    path('', include('reservations.urls'), name='reservations_urls'),
 ]
 
