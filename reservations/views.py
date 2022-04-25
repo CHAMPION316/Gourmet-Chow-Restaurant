899e07307a80a10182c86dc47b6bdcd22ca293db
C316 <royer.seguracalderon@gmail.com>
2022-04-25 03:17:06 +0000

    Create handler500 function

--- reservations/views.py
+++ reservations/views.py
@@ -13,3 +13,17 @@ def handler404(request, *args, **argv):
     response = render(request, 'restaurant/not_found.html', context)
     response.status_code = 404
     return response
+
+
+def handler500(request, *args, **argv):
+    """
+    Function to enable customizing 404 error page
+    in production environment if booking is duplicate.
+    """
+    bookings = Booking.objects.all()
+    context = {
+        'bookings': bookings
+    }
+    response = render(request, 'restaurant/duplicate_booking.html', context)
+    response.status_code = 500
+    return response
