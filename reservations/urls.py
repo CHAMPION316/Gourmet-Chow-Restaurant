16207d6afdd910a298ac8b0f09d7aa6f10ac0c56
C316 <royer.seguracalderon@gmail.com>
2022-04-25 01:43:20 +0000

    update paths

--- reservations/urls.py
+++ reservations/urls.py
@@ -4,12 +4,10 @@ different pages in the app.
 """
 
 from django.urls import path
-from restaurant import views
+from reservations import views
 
 
 urlpatterns = [
     path('', views.view_home, name='home'),
-    path('menu/', views.view_menu, name='menu'),
     path('add_booking/', views.add_booking, name='add_booking'),
-    path('contact', views.contact, name='contact'),
 ]
