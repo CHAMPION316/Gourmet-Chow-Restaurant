66ff1b484b9e29b049fe378a9e95c17331532eb3
C316 <royer.seguracalderon@gmail.com>
2022-04-25 22:14:42 +0000

    delete element from page

--- reservations/views.py
+++ reservations/views.py
@@ -1,6 +1,11 @@
+"""
+VIEWS
+"""
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Booking
+from .forms import BookingForm
 
 def handler404(request, *args, **argv):
     """
