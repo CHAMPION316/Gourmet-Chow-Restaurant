3d4ba4c1145928a9f6f1a73829138baa94fc048c
C316 <royer.seguracalderon@gmail.com>
2022-04-25 01:58:05 +0000

    Add variables with models for Booking class

--- reservations/models.py
+++ reservations/models.py
@@ -7,3 +7,7 @@ class Booking(models.Model):
     Class for booking model
     in database and for booking form.
     """
+    user = models.ForeignKey(User, null=True, blank=True)
+    customer_name = models.CharField(max_length=20, null=True)
+    reservation_date_and_time = models.DateTimeField(null=True)
+                             
