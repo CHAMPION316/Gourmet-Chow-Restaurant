a116c60ea3aea1b36557bdb3dca3dfbd9cc64155
C316 <royer.seguracalderon@gmail.com>
2022-04-26 04:44:08 +0000

    Try to fix crispy issues for page rendering

--- matrix_2022/settings.py
+++ matrix_2022/settings.py
@@ -59,6 +59,16 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
+# MESSAGE_TAGS = {
+#     messages.DEBUG: 'alert-info',
+#     messages.INFO: 'alert-info',
+#     messages.SUCCESS: 'alert-success',
+#     messages.WARNING: 'alert-warning',
+#     messages.ERROR: 'alert-danger',
+# }
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPUS_DOMINUS_LOCALIZE = False
 
 MIDDLEWARE = [
