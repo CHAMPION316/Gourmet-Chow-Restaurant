d06fedaef66515d87cd7ff4c74d883193350e1c5
C316 <royer.seguracalderon@gmail.com>
2022-08-01 17:19:39 +0000

    change APP_DIRS to true

--- reservations/views.py
+++ reservations/views.py
@@ -1,6 +1,6 @@
-# """
-# VIEWS
-# """
+"""
+VIEWS
+"""
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
