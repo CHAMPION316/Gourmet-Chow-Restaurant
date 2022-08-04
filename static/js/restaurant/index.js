eaa739df0d34f3095b4e724c640ff94bb328e457
C316 <royer.seguracalderon@gmail.com>
2022-08-04 00:53:37 +0000

    Create a js file for index.html to target DOM

---
+++ static/js/restaurant/index.js
@@ -0,0 +1,21 @@
+// function myFunction(x) {
+//   if x < 991 { // If media query matches
+//     Element.class.remove.("fs-4"); 
+//   } else {
+//    return False
+//   };
+// };
+
+// var x = window.innerWidth("(max-width: 991px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
+
+
+// function resize() {
+//   var smallText = document.querySelector('p');
+//   if window.innerWidth < 991 {
+//     element.classList.remove("fs-4");
+//   } else {
+//     return false
+//   }
+// }
