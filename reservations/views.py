21bc811524cec1cf8812d0c39fa8c9f68849f57c
C316 <royer.seguracalderon@gmail.com>
2022-08-08 01:18:03 +0000

    Uncomment error handler500/400

--- reservations/views.py
+++ reservations/views.py
@@ -7,32 +7,32 @@ from django.contrib import messages
 from .models import Booking
 from .forms import BookingForm
 
-# def handler404(request, *args, **argv):
-#     """
-#     A Function to trigger a 404 error
-#     page in production environment if booking is not found.
-#     """
-#     form = BookingForm()
-#     context = {
-#         'form': form
-#         }
-#     response = render(request, 'restaurant/not_found.html', context)
-#     response.status_code = 404
-#     return response
+def handler404(request, *args, **argv):
+    """
+    A Function to trigger a 404 error
+    page in production environment if booking is not found.
+    """
+    form = BookingForm()
+    context = {
+        'form': form
+        }
+    response = render(request, 'restaurant/not_found.html', context)
+    response.status_code = 404
+    return response
 
 
-# def handler500(request, *args, **argv):
-#     """
-#     Function to enable customizing 404 error page
-#     in production environment if booking is duplicate.
-#     """
-#     bookings = Booking.objects.all()
-#     context = {
-#         'bookings': bookings
-#     }
-#     response = render(request, 'restaurant/duplicate_booking.html', context)
-#     response.status_code = 500
-#     return response
+def handler500(request, *args, **argv):
+    """
+    Function to enable customizing 404 error page
+    in production environment if booking is duplicate.
+    """
+    bookings = Booking.objects.all()
+    context = {
+        'bookings': bookings
+    }
+    response = render(request, 'restaurant/duplicate_booking.html', context)
+    response.status_code = 500
+    return response
 
 
 def view_home(request):
